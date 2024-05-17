b5df504e293bdff7496f1f3a34b1d35865d82c70
S-hre29ya07 <singh.shreya2907@gmail.com>
2024-05-17 12:23:11 +0530

    closures

--- index.js
+++ index.js
@@ -8,14 +8,28 @@
 // }
 // count();
 
-function count(){
-    for (let i=1; i<=5; i++){
-        function x(){
-            setTimeout(function(){
-                console.log(i)
-            }, i*1000)
-        }
-        x(i)
+// function count(){
+//     for (let i=1; i<=5; i++){
+//         function x(){
+//             setTimeout(function(){
+//                 console.log(i)
+//             }, i*1000)
+//         }
+//         x(i)
+//     }
+// }
+// count();
+
+
+// Here b is called parameter
+function outer(b){
+    function inner(){
+        console.log(a,b)
     }
+    let a = 10;
+    inner();
 }
-count();
+var z = outer(20);
+// Here the string is called argument
+z();
+
