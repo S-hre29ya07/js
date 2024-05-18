4e8ea684f7ac3cce5059eb63c89edbdde0b6c74d
S-hre29ya07 <singh.shreya2907@gmail.com>
2024-05-18 12:12:55 +0530

    sum array

--- index.js
+++ index.js
@@ -1,49 +1,8 @@
-// function count(){
-//     for (let i = 1; i<=5; i++){
-//         setTimeout(function(){
-//             console.log(i)
-//         }, i*1000)
-//     }
-//     console.log("shreya")
-// }
-// count();
-
-// function count(){
-//     for (let i=1; i<=5; i++){
-//         function x(){
-//             setTimeout(function(){
-//                 console.log(i)
-//             }, i*1000)
-//         }
-//         x(i)
-//     }
-// }
-// count();
-
-
-// Here b is called parameter
-// function outer(b){
-//     function inner(){
-//         console.log(a,b)
-//     }
-//     let a = 10;
-//     inner();
-// }
-// var z = outer(20);
-// // Here the string is called argument
-// z();
-
-function outest(){
-    var c = 2000;
-    function outer(b){
-        function inner(){
-            console.log(a,b,c)
-        }
-        var a = 10;
-        inner();
+function findSum(arr){
+    var count = 0;
+    for (var i =0; i<arr.length; i++){
+        count= count+arr[i]
     }
-    outer("Shreya outer");
+    console.log(count)
 }
-var a = 150;
-var z = outest();
-z();
+findSum([1,2,3])
