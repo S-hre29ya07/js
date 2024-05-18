343cc9b972d73ecb4ab3efdca746d27c2acfbb66
S-hre29ya07 <singh.shreya2907@gmail.com>
2024-05-18 12:22:19 +0530

    sum of two numbers

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-function findSum(arr){
-    var count = 0;
-    for (var i =0; i<arr.length; i++){
-        count= count+arr[i]
-    }
-    console.log(count)
+// function findSum(arr){
+//     var count = 0;
+//     for (var i =0; i<arr.length; i++){
+//         count= count+arr[i]
+//     }
+//     console.log(count)
+// }
+// findSum([1,2,3])
+
+// function findSum(arr){
+//     var count =0;
+//     for (var i =0; i<arr.length; i++){
+//         count=count+arr[i]
+//     }
+//     return count;
+// }
+// const arr = [1,2,3]
+// var ans = findSum(arr);
+// console.log(ans)
+function findSum(a,b){
+    return (a+b);
 }
-findSum([1,2,3])
+ans = findSum(1,2);
+console.log(ans)
