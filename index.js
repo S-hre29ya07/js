b0fe045a9b3f11763ad5aea1ef5bf7d7e8864f90
S-hre29ya07 <singh.shreya2907@gmail.com>
2024-05-18 12:29:11 +0530

    largest number in array

--- index.js
+++ index.js
@@ -17,8 +17,21 @@
 // const arr = [1,2,3]
 // var ans = findSum(arr);
 // console.log(ans)
-function findSum(a,b){
-    return (a+b);
+// function findSum(a,b){
+//     return (a+b);
+// }
+// ans = findSum(1,2);
+// console.log(ans)
+
+function findMax(arr){
+    num = arr[0];
+    for (var i =1; i<arr.length; i++){
+        if(arr[i]>num){
+            num = arr[i]
+        }
+    }
+    return num
 }
-ans = findSum(1,2);
+const arr = [1,2,3,7];
+ans = findMax(arr)
 console.log(ans)
