27abf47ca0a226357c3d21afb2294e7e887aecfa
S-hre29ya07 <singh.shreya2907@gmail.com>
2024-05-21 13:24:24 +0530

    find index of NaN in array

--- DSA.js
+++ DSA.js
@@ -1,14 +1,28 @@
 // Count number of arrays in array;
 
-function countArr(arr){
-    count =0;
-    for (var i of arr){
-        if(Array.isArray(i)){
-            count++
+// function countArr(arr){
+//     count =0;
+//     for (var i of arr){
+//         if(Array.isArray(i)){
+//             count++
+//         }
+//     }
+//     return count
+// }
+// const arr = [2,8,[6,3,3],[4],5,[3,4,[5,4]]]
+// ans = countArr(arr)
+// console.log(ans)
+
+// Write a JavaScript program to get all the indexes where NaN is found in a given array of numbers and NaN.
+function findNaN(arr){
+    var ans=[];
+    for (let i in arr){
+        if (Number.isNaN(arr[i])){
+            ans.push(parseInt(i))
         }
     }
-    return count
+    return ans
 }
-const arr = [2,8,[6,3,3],[4],5,[3,4,[5,4]]]
-ans = countArr(arr)
+const arr = [2, 4, NaN, 16, 32, NaN]
+ans = findNaN(arr)
 console.log(ans)
